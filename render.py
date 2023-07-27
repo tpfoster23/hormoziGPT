f293eb6e99ec52c755bdd5730b04a33dd76de8ed
tpfoster23 <140723156+tpfoster23@users.noreply.github.com>
2023-07-27 14:42:04 +0100

    made changes color

--- render.py
+++ render.py
@@ -7,7 +7,7 @@ bot_msg_container_html_template = '''
     <div style="width: 20%; display: flex; justify-content: center">
         <img src="https://yt3.googleusercontent.com/ixwBtVrollE0Z5nA5YPHrnkKQoK09Evbe4gWCvJlleB2rFERDz3m2Jynhc3sGBE-EnzbH6ov=s176-c-k-c0x00ffffff-no-rj" style="max-height: 50px; max-width: 50px; border-radius: 50%;">
     </div>
-    <div style="width: 80%;">
+    <div style="width: 80%;color:black;">
         $MSG
     </div>
 </div>
@@ -15,7 +15,7 @@ bot_msg_container_html_template = '''
 
 user_msg_container_html_template = '''
 <div style='background-color: #FFFFFF; padding: 10px; border-radius: 5px; margin-bottom: 10px; display: flex'>
-    <div style="width: 78%">
+    <div style="width: 78%;color:black;">
         $MSG
     </div>
     <div style="width: 20%; margin-left: auto; display: flex; justify-content: center;">
